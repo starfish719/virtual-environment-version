aaefabade3b812b2c3699c5b81b69f5f9f9b456e
toda <starfish719@gmail.com>
2020-03-14 19:42:36 +0900

    test: 💍 add test pattern

--- __tests__/main.test.ts
+++ __tests__/main.test.ts
@@ -2,7 +2,18 @@ import * as process from 'process'
 import * as cp from 'child_process'
 import * as path from 'path'
 
-// shows how the runner will run a javascript action with env / stdout protocol
+import {createCommand} from '../src/createCommand'
+
+describe('createCommand', () => {
+  it('python', () => {
+    expect(createCommand('python')).toEqual('python --version')
+  })
+
+  it('ruby', () => {
+    expect(createCommand('ruby')).toEqual('ruby --version')
+  })
+})
+
 test('test runs', () => {
   process.env['INPUT_TARGET'] = 'python'
   const ip = path.join(__dirname, '..', 'lib', 'main.js')
