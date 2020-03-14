f7f5334584dbf860c501d039336d0a9c6ea9e6d4
toda <starfish719@gmail.com>
2020-03-14 18:59:39 +0900

    refactor: 💡 test

--- src/main.ts
+++ src/main.ts
@@ -3,19 +3,6 @@ import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    const options = {
-      listeners: {},
-      cwd: './lib'
-    }
-    options.listeners = {
-      stdout: (data: Buffer) => {
-        return data.toString()
-      },
-      stderr: (data: Buffer) => {
-        return data.toString()
-      }
-    }
-    options.cwd = './lib'
     await exec.exec('python --version')
   } catch (error) {
     core.setFailed(error.message)
