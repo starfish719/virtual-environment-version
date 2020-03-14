77ff42b1b2ddab279661aafbd3c5736b467e1b92
toda <starfish719@gmail.com>
2020-03-14 19:10:48 +0900

    feat: 🎸 use input target

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,13 @@
 import * as core from '@actions/core'
 import * as exec from '@actions/exec'
 
+import {createCommand} from './createCommand'
+
 async function run(): Promise<void> {
   try {
-    await exec.exec('python --version')
+    const target: string = core.getInput('target')
+    const execCommand: string = createCommand(target)
+    await exec.exec(execCommand)
   } catch (error) {
     core.setFailed(error.message)
   }
