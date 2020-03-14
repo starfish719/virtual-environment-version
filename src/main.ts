8ea0b524402612660ac149f6af3ca01cf375b5a9
toda <starfish719@gmail.com>
2020-03-14 10:47:30 +0900

    fix: 🐛 fix options

--- src/main.ts
+++ src/main.ts
@@ -3,21 +3,18 @@ import * as exec from '@actions/exec'
 
 async function run(): Promise<void> {
   try {
-    let myOutput = ''
-    let myError = ''
-
     const options = {
       listeners: {}
     }
     options.listeners = {
       stdout: (data: Buffer) => {
-        myOutput += data.toString()
+        return data.toString()
       },
       stderr: (data: Buffer) => {
-        myError += data.toString()
+        return data.toString()
       }
     }
-    await exec.exec('python --version', [], options)
+    await exec.exec('python', ['--version'], options)
   } catch (error) {
     core.setFailed(error.message)
   }
